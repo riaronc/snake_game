0316cec6af44a6f6fd412ed9c11dac2f2318b828
riaronc <riaronc@gmail.com>
2021-01-13 17:13:20 +0200

    create snake class git push

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from turtle import Turtle, Screen
+import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.title("Snake game")
+screen.tracer(0)
+
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(1)
+    screen.update()
+    snake.move_snake()
+
+screen.exitonclick()
