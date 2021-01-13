e5342571e16d4b6e38e564083299cb66a0657572
riaronc <riaronc@gmail.com>
2021-01-13 17:33:29 +0200

    add keyboard control

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ screen.tracer(0)
 
 
 snake = Snake()
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
 
 game_is_on = True
 while game_is_on:
     time.sleep(1)
     screen.update()
-    snake.move_snake()
+    snake.move()
 
 screen.exitonclick()
